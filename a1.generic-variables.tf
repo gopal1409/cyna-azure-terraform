08a54bfcd821375d0a79144943953f58baf81104
gopal <gopal1409@gmail.com>
2024-07-12 12:37:52 +0530

    varaibles

---
+++ a1.generic-variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type = string #bool map list of string
+  default = "cyan-rg"
+}
+
+variable "resource_group_location" {
+  type = string 
+  default = "eastus2"
+}
