e5ef93ce7c0fe5c50723544794e5e2bbe55fa7a4
gopal <gopal1409@gmail.com>
2024-07-15 12:02:52 +0530

    added vm

---
+++ web-vm.tf
@@ -0,0 +1,29 @@
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+  for_each = var.web_linux_instance_count
+  name                = "${local.resoure_name_prefix}-nic-${each.key}"
+   #sap-hr-vnet
+  location            = azurerm_resource_group.rg.location #us2
+  resource_group_name = azurerm_resource_group.rg.name #cyan-rg
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password = "Admin@12345678"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.web_linux_nic[each.key].id,
+  ]
+
+  
+  os_disk {
+    name = "osdisk-${each.key}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  custom_data = filebase64("{path.module}/app/app.sh")
+}
