8d750743ebccb04c2a46ba5b0efb94714c8ea42a
gopal <gopal1409@gmail.com>
2024-07-15 11:18:45 +0530

    added two subnet

--- a4.subnet.tf
+++ a4.subnet.tf
@@ -12,3 +12,38 @@ resource "azurerm_subnet" "dbsubnet" {
   virtual_network_name = azurerm_virtual_network.vpc.name 
   address_prefixes = var.db_subnet_address
 }
+#next we will create nsg\
+#lets create nsg and open the port 
+resource "azurerm_network_security_group" "web_subnet_nsg" {
+  name = "${local.resoure_name_prefix}-nsg"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+#this nsg need to be part of your subnet also
+resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
+  subnet_id = azurerm_subnet.websubnet.id
+  network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
+}
+locals {
+  web_inbound_ports_maps = { #name is web_inbound_ports_maps
+    "110" : "22", #expression in key value format
+    "120" : "80",
+    "130" : "443"
+  }
+}
+#create rules to open port 
+resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
+    for_each = local.web_inbound_ports_maps
+  name                        = "Rule-Port-${each.value}" #rule-port-22
+  priority                    = each.key #110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value #22 80 443
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name 
+  network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name 
+}
